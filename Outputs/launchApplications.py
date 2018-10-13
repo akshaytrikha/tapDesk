ee49687a1811ebd15d9b679f51081544e74ed2a1
akshaytrikha <akshaytrikha@gmail.com>
2018-10-13 06:06:50 -0700

    Script for outputs

---
+++ Outputs/launchApplications.py
@@ -0,0 +1,27 @@
+import webbrowser
+import subprocess
+import os
+
+# for playing music
+# webbrowser.open('https://www.youtube.com/watch?v=FNKPYhXmzoE')
+
+# for homework
+# webbrowser.open('https://drive.google.com/drive/u/0/my-drive')
+# webbrowser.open('https://mail.google.com/mail/u/0/')
+# webbrowser.open('https://sakai.claremont.edu/portal')
+# webbrowser.open('https://www.gradescope.com/')
+
+# for coding
+# webbrowser.open('https://github.com/')
+# subprocess.call(["/usr/bin/open", "-W", "-n", "-a", "/Applications/Atom.app"])
+# subprocess.call(["/usr/bin/open", "-W", "-n", "-a", "/Applications/Spotify.app"])
+# webbrowser.open('https://stackoverflow.com/')
+
+# opening spotify
+# subprocess.call(["/usr/bin/open", "-W", "-n", "-a", "/Applications/Spotify.app"])
+
+# ordering Dominos
+# webbrowser.open('https://pizza.dominos.com/california/claremont/366-w-foothill-blvd/')
+
+# call Adrian on Facetime
+os.popen('open facetime://+19094809798')
